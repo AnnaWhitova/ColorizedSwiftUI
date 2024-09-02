83cd340f9ccd445cab821d51f0ddf1442e971adf
AnnaWhitova <159627315+AnnaWhitova@users.noreply.github.com>
2024-09-02 11:44:08 +0300

    work with UI and App logic

--- ColorizedSwiftUI/ContentView.swift
+++ ColorizedSwiftUI/ContentView.swift
@@ -8,17 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var redSliderValue = Double.random(in: 0...1)
+    @State private var greenSliderValue = Double.random(in: 0...1)
+    @State private var blueSliderValue = Double.random(in: 0...1)
+   
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.indigo
+                .ignoresSafeArea()
+            VStack {
+                ColoredRectangleView(redSliderValue: $redSliderValue, greenSliderValue: $greenSliderValue, blueSliderValue: $blueSliderValue)
+                    .padding(.bottom, 30)
+                SliderView(sliderValue: $redSliderValue, color: Color.red)
+                SliderView(sliderValue: $greenSliderValue, color: Color.green)
+                SliderView(sliderValue: $blueSliderValue, color: Color.blue)
+                
+            }
+            .padding(.bottom, 400)
+            .padding()
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct ColoredRectangleView: View {
+    @Binding var redSliderValue: Double
+    @Binding var greenSliderValue: Double
+    @Binding var blueSliderValue: Double
+
+    var body: some View {
+       RoundedRectangle(cornerRadius: 20)
+            .frame(width: 350, height: 150)
+            .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color.white, lineWidth: 4)
+            )
+            .foregroundStyle(Color(red: redSliderValue, green: greenSliderValue, blue: blueSliderValue))
+           
+    }
+}
+
+struct SliderView: View {
+    @Binding var sliderValue: Double
+    var color: Color
+    
+    var body: some View {
+        HStack {
+            Text(String(format:"%.2f", sliderValue))
+                .foregroundStyle(.white)
+            Slider(value: $sliderValue, in: 0...1, step: 0.1)
+                .tint(color)
+        }
+        
+    }
 }
